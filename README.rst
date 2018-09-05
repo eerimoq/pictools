e1fc9dfd49f80b5dd92e59b3cb5245bb3b4dd324
Erik Moqvist <erik.moqvist@gmail.com>
2018-09-05 20:17:57 +0200

    Initial comit with code from the Simba repository.

---
+++ README.rst
@@ -0,0 +1,37 @@
+About
+=====
+
+PIC32 tools.
+
+- PIC32 programming.
+
+Project homepage: https://github.com/eerimoq/pic32tools
+
+Documentation: http://pic32tools.readthedocs.org/en/latest
+
+Installation
+============
+
+.. code-block:: python
+
+    pip install pic32tools
+
+Example usage
+=============
+
+ToDo
+
+Contributing
+============
+
+#. Fork the repository.
+
+#. Install prerequisites.
+
+   .. code-block:: text
+
+      pip install -r requirements.txt
+
+#. Implement the new feature or bug fix.
+
+#. Create a pull request.
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+import re
+
+
+def find_version():
+    return re.search(r"^__version__ = '(.*)'$",
+                     open('pic32tools.py', 'r').read(),
+                     re.MULTILINE).group(1)
+
+
+setup(name='pic32tools',
+      version=find_version(),
+      description='PIC32 tools.',
+      long_description=open('README.rst', 'r').read(),
+      author='Erik Moqvist',
+      author_email='erik.moqvist@gmail.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      keywords=['pic32', 'programmer'],
+      url='https://github.com/eerimoq/pic32tools',
+      install_requires=[
+          'pyserial'
+      ],
+      entry_points = {
+          'console_scripts': ['pic32tools=pic32tools:_main']
+      })
