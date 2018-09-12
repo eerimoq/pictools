905a4f663ce0a1ac2112e29980c9a9d41842a620
Erik Moqvist <erik.moqvist@gmail.com>
2018-09-12 22:47:56 +0200

    Documentation and benchmark test.

--- tests/target.py
+++ tests/target.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
 import subprocess
+import bincopy
 
 
-def run(command):
-    command = 'python3 -u -m pictools --port /dev/arduino ' + command
-
-    print()
-    print("# Running '{}'.".format(command))
-    print()
-
+def run(command, timed=False):
     command = command.split(' ')
+    command = ['python3', '-m', 'pictools', '-p', '/dev/arduino'] + command
+
+    if timed:
+        command = ['/usr/bin/env', 'bash', '-c',
+                   'time ' + ' '.join(command)]
+
+    print("> {}".format(' '.join(command)))
 
     subprocess.check_call(command)
+
+    print()
 
 
 def main():
@@ -23,10 +27,19 @@ def main():
     run('flash_write --chip-erase --verify memory.s19')
     run('flash_read 0x1d000000 0x1000 memory.s19')
     run('flash_erase 0x1d000000 0x1000')
-    run('reset')
     run('configuration_print')
     run('device_id_print')
     run('udid_print')
+    run('reset')
+
+    binfile = bincopy.BinFile()
+    binfile.add_binary(0x40000 * b'\x00', 0x1d000000)
+    binfile.add_binary(0x1800 * b'\x00', 0x1fc00000)
+
+    with open('zeros.s19', 'w') as fout:
+        fout.write(binfile.as_srec())
+
+    run('flash_write --chip-erase zeros.s19', timed=True)
 
 
 if __name__ == '__main__':
